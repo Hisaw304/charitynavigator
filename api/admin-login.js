120f2490204fa418f9e189d25f4227f9f5ee6bbc
Hisaw <hisawsarah321@gmail.com>
2026-06-01 09:23:36 -0700

    console log updated

--- api/admin-login.js
+++ api/admin-login.js
@@ -7,12 +7,6 @@ export default async function handler(req, res) {
 
   const { username, password } = req.body;
 
-  console.log("FORM USERNAME:", username);
-  console.log("FORM PASSWORD:", password);
-
-  console.log("ENV USERNAME:", process.env.ADMIN_USERNAME);
-  console.log("ENV PASSWORD:", process.env.ADMIN_PASSWORD);
-
   if (
     username === process.env.ADMIN_USERNAME &&
     password === process.env.ADMIN_PASSWORD
